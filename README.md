9de74e2e7562e1db927a0258ad12dcce2e6e7038
nazimfeni <mdnazimuddin77@gmail.com>
2023-06-13 06:08:44 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# flutter_api
+
+A new Flutter project.
+##  Tutorial link
+https://www.youtube.com/watch?v=ExPFnu8Dm40
+
+## API fetch link 
+https://randomuser.me/documentation
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/screen/home.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Rest API Call"),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          final name = user['name']['first'];
+          final email = user['email'];
+          final imageurl = user['picture']['thumbnail'];
+          return ListTile(
+            leading: ClipRRect(child: Image.network(imageurl),
+                borderRadius: BorderRadius.circular(100),
+            ),
+            title: Text(name),
+            subtitle: Text(email),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchusers,
+      ),
+    );
+  }
+
+  void fetchusers() async {
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    print("fetch user completed");
+  }
+}
